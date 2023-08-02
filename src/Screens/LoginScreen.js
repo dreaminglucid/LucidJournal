665c2ee159c516106ac81489be11c847404909c5
cp-james-harbeck <jamesharbeck@cipherproxy.com>
2023-08-02 06:59:26 -0400

    cleanup login page  styles.

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Button, View, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { AppleAuthContext } from '../Contexts/AppleAuthContext';
 import { ThemeContext } from '../Contexts/ThemeContext';
 import { useNavigation } from '@react-navigation/native';
@@ -19,20 +19,62 @@ export default function LoginScreen() {
   }
 
   return (
-    <View style={[styles.container, {backgroundColor: theme.colors.background}]}>
-      <Button
-        title="Login with Apple"
-        color={theme.colors.text}
-        onPress={handleLogin}
-      />
+    <View style={styles(theme).container}>
+      <View style={styles(theme).heroCard}>
+        <Text style={styles(theme).heroTitle}>Lucid Journal</Text>
+        <Text style={styles(theme).heroSubtitle}>Your AI Dream Journal Guide</Text>
+      </View>
+      <TouchableOpacity style={styles(theme).loginButton} onPress={handleLogin}>
+        <Text style={styles(theme).loginButtonText}>Login with Apple</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = (theme) => StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: theme.colors.background,
+    alignItems: 'center',
     justifyContent: 'center',
     padding: 16,
+  },
+  heroCard: {
+    backgroundColor: theme.colors.card,
+    borderRadius: 20,
+    padding: 20,
+    marginBottom: 20,
+    width: '90%',
+    shadowColor: "#123",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.26,
+    shadowRadius: 2,
+    elevation: 4,
+  },
+  heroTitle: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: theme.colors.text,
+    textAlign: 'center',
+  },
+  heroSubtitle: {
+    fontSize: 18,
+    color: theme.colors.text,
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  loginButton: {
+    backgroundColor: '#00ADB5',
+    padding: 15,
+    borderRadius: 20,
+    width: '90%',
+    alignItems: 'center',
+  },
+  loginButtonText: {
+    color: theme.colors.background,
+    fontSize: 18,
   },
 });
